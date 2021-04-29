12fe20ce20c3cfdb107f1f18908ca80b33ff2107
sktharani <83069040+sktharani@users.noreply.github.com>
2021-04-29 15:45:45 +0530

    Add files via upload

---
+++ app-mern-starter/build/precache-manifest.99d99ecdc1b189039d9550ade7874a3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163da1d5005209673e3e0afd716a226d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c796f4ea93ebc0834fd",
+    "url": "/static/css/main.18ea2eca.chunk.css"
+  },
+  {
+    "revision": "f986c0ee71900849b798",
+    "url": "/static/js/2.bc320995.chunk.js"
+  },
+  {
+    "revision": "3493125ba862a8e2c7d04aaec8baffd8",
+    "url": "/static/js/2.bc320995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c796f4ea93ebc0834fd",
+    "url": "/static/js/main.d535fdbb.chunk.js"
+  },
+  {
+    "revision": "fa430200eae998bbeca5",
+    "url": "/static/js/runtime-main.ee4f1273.js"
+  }
+]);
